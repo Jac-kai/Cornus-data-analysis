3d2b21bf435cad8545d8b25b65ed33d3cf04bdec
Zack <jackliu122412@gmail.com>
2026-04-02 11:16:17 +0800

    Update TrendencyCore indentation

--- MetaUnits/TrendencyCore.py
+++ MetaUnits/TrendencyCore.py
@@ -11,9 +11,8 @@ from Cornus.MetaUnits.ClarityCore import ClarityCore
 
 BASED_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 PLOT_DIR = os.path.join(BASED_PATH, "Features_Plot")
+
 os.makedirs(PLOT_DIR, exist_ok=True)
-
-
 # -------------------- TrendencyCore --------------------
 class TrendencyCore:
     """
